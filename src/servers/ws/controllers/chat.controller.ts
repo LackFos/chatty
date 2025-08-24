66a3b303455d108ac80b527893132e129cbf7b0f
LackFos <LackFosisOnline@gmail.com>
2025-08-24 17:56:17 +0700

    Fix inbox feature not working

--- src/servers/ws/controllers/chat.controller.ts
+++ src/servers/ws/controllers/chat.controller.ts
@@ -1,24 +1,23 @@
 import WebSocket from 'ws';
 
 import ChatModel from '@/models/chat.model';
+import UserModel from '@/models/user.model';
 import { subscribeContext, userContext } from '@/servers/ws/server';
 import { ChatMessageInterface } from '@/servers/ws/dtos/chat.dto';
 
 export const createChat = async (ws: WebSocket, message: ChatMessageInterface) => {
   const context = userContext.get(ws);
-  const user = context.user;
+  const user = context.user!;
 
-  if (!user) {
-    return ws.close();
-  }
+  const receiver = await UserModel.findOne({ email: message.to });
 
-  if (!message.to) {
-    return ws.close();
+  if (!receiver) {
+    return ws.send(JSON.stringify({ type: 'error', message: 'User not found' }));
   }
 
   await ChatModel.create({ sender: user._id, receiver: user._id, text: message.text });
 
   // Send message to the target user
-  const inboxTopic = `user.${message.to}.inbox`;
+  const inboxTopic = `user.${receiver._id}.inbox`;
   subscribeContext.publish(inboxTopic, message.text);
 };
